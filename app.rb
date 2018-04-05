79aa2d77800d4006e1e4a0f76af1edd679f351f9
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-05 20:01:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+  
+  get '/square/:number' do
     
 
 end
