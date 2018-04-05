8292f17ca1ff8245f9aaacd310375bb3ae0a43b3
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-05 20:27:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,8 +15,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
+    @phrases = ""
     @number.times do 
-      puts @phrase
+      @phrases << @phrase
     end
   end
 
